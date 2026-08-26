2d439abaaaa19183fcfafed6e3342681520ccf33
tkjelds <tkjelds385@gmail.com>
2026-08-26 16:12:23 +0200

    I am dumb and typed the wrong key

--- keyboards/splitkb/halcyon/lily58/keymaps/tkjelds/keymap.c
+++ keyboards/splitkb/halcyon/lily58/keymaps/tkjelds/keymap.c
@@ -50,8 +50,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // keyencoder
     [_SYMBOL_MAC] = LAYOUT(
         KC_TRNS	, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,              KC_TRNS	, KC_TRNS, KC_TRNS, KC_TRNS	, KC_TRNS	, KC_TRNS	,
-        L_TAB	   , DK_GRV	   , DK_LABK   , DK_RABK   , DK_MINS   , DK_SLSH	       , DK_CIRC   , M_LCBR , M_RCBR , M_DLLR , KC_NO	   , KC_NO ,
-        KC_QUOT , DK_EXLM   , DK_ASTR   , DK_QUES   , DK_EQL	   , DK_AMPR	       , M_PIPE , DK_LPRN   , DK_RPRN   , DK_SCLN   , KC_GRV , KC_NO ,
+        L_TAB	   , DK_GRV	   , DK_LABK   , DK_RABK   , DK_MINS   , DK_SLSH	       , DK_CIRC   , M_LCBR , M_RCBR , KC_GRV , KC_NO	   , KC_NO ,
+        KC_QUOT , DK_EXLM   , DK_ASTR   , DK_QUES   , DK_EQL	   , DK_AMPR	       , M_PIPE , DK_LPRN   , DK_RPRN   , DK_SCLN   , M_BSLH , KC_NO ,
         KC_DQUO, DK_TILD   , DK_PLUS   , DK_LBRC   , DK_RBRC   , DK_PERC	, KC_F18   , KC_F19  , M_AT , DK_COLN   , DK_HASH   , KC_TRNS   , KC_TRNS   , KC_TRNS  ,
                           KC_NO , KC_TRNS	, MEDIA,     KC_TRNS	,               KC_TRNS	 , KC_TRNS	, KC_TRNS	 , KC_TRNS	 ),
 
