99fa2110de358fa97040b9c42860846b7dcbd722
tkjelds <tkjelds385@gmail.com>
2026-08-26 16:37:49 +0200

    Fixed dollar sign on mac for corne.

--- keyboards/splitkb/halcyon/corne/keymaps/tkjelds/keymap.c
+++ keyboards/splitkb/halcyon/corne/keymaps/tkjelds/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         DK_DQUO    , DK_TILD   , DK_PLUS   , DK_LBRC   , DK_RBRC   , DK_PERC	       , DK_AT	   , DK_COLN   , DK_HASH   , KC_TRNS   , KC_TRNS   , KC_TRNS  ,
                                              KC_TRNS   , MEDIA	   , KC_SPC	           , KC_ENT	   , MEDIA	   , KC_RALT)  ,
     [_SYMBOL_MAC] =  LAYOUT_split_3x6_3(
-        L_TAB	   , DK_GRV	   , DK_LABK   , DK_RABK   , DK_MINS   , DK_SLSH	       , DK_CIRC   , M_LCBR    , M_RCBR    , M_DLLR    , KC_NO	   , KC_NO ,
+        L_TAB	   , DK_GRV	   , DK_LABK   , DK_RABK   , DK_MINS   , DK_SLSH	       , DK_CIRC   , M_LCBR    , M_RCBR    , KC_GRV    , KC_NO	   , KC_NO ,
         DK_QUOT	   , DK_EXLM   , DK_ASTR   , DK_QUES   , DK_EQL	   , DK_AMPR	       , M_PIPE    , DK_LPRN   , DK_RPRN   , DK_SCLN   , M_BSLH    , KC_NO ,
         DK_DQUO    , DK_TILD   , DK_PLUS   , DK_LBRC   , DK_RBRC   , DK_PERC	       , M_AT      , DK_COLN   , DK_HASH   , KC_TRNS   , KC_TRNS   , KC_TRNS  ,
                                              KC_TRNS   , MEDIA	   , KC_SPC	           , KC_ENT	   , MEDIA	   , KC_RALT)  ,
